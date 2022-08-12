6df7d9251ceca545b8249805c66888048d9dc94f
Adam Brock <a.paulbrock@gmail.com>
2022-08-11 23:25:29 -0500

    The user can now update and delete their posts. I also removed caching on most of the major queries, because it cause it interfered with the user's viewing experience after updating or deleting their posts.

--- client/src/ReactComponents+Pages/Pages/EditPostForm/index.js
+++ client/src/ReactComponents+Pages/Pages/EditPostForm/index.js
@@ -28,28 +28,27 @@ const EditPostForm = () => {
       postText: post?.postText,
       expectedTradeCompensation: post?.expectedTradeCompensation,
     });
-    console.log(formState);
     setCharacterCount({
       expectedTradeCompensation: post?.postTitle?.length,
       postTitle: post?.postText?.length,
       postText: post?.expectedTradeCompensation?.length,
     });
   }, [data]);
-  const [addPost, { error }] = useMutation(EDIT_POST);
+  const [updatePost, { error }] = useMutation(EDIT_POST);
 
   let navigate = useNavigate();
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      const { data } = await addPost({
+      const { data } = await updatePost({
         variables: {
           ...formState,
+          postId,
         },
       });
-      console.log(data);
       if (data) {
-        navigate(`/posts/${data.addPost._id}`, { replace: true });
+        navigate(`/posts/${postId}`, { replace: true });
       }
     } catch (err) {
       console.error(err);
@@ -66,104 +65,104 @@ const EditPostForm = () => {
       [name]: value,
     });
   };
-
-  return (
-    <div className="mb-3">
-      <h3>Update your Listing</h3>
-
-      {Auth.loggedIn() ? (
-        loading ? (
-          <h2>Loading...</h2>
-        ) : (
-          <>
-            <form
-              className="flex-row justify-center justify-space-between-md align-center"
-              onSubmit={handleFormSubmit}
-            >
-              <div className="col-12">
-                <h6>The name of your listed item:</h6>
-                <textarea
-                  id="postTitle"
-                  name="postTitle"
-                  placeholder="What item are you offering?"
-                  value={formState.postTitle}
-                  className="form-input w-100"
-                  style={{ lineHeight: "1.5", resize: "vertical" }}
-                  onChange={handleChange}
-                ></textarea>
-                <p
-                  className={`m-0 ${
-                    characterCount.postTitle >= 150 || error
-                      ? "text-danger"
-                      : ""
-                  }`}
-                >
-                  Character Count: {characterCount.postTitle}/150
-                </p>
-                <br />
-                <h6>Listing Details:</h6>
-                <textarea
-                  id="postText"
-                  name="postText"
-                  placeholder="Type any relevant information about your item here..."
-                  value={formState.postText}
-                  className="form-input w-100"
-                  style={{ lineHeight: "1.5", resize: "vertical" }}
-                  onChange={handleChange}
-                ></textarea>
-                <p
-                  className={`m-0 ${
-                    characterCount.postText >= 280 || error ? "text-danger" : ""
-                  }`}
-                >
-                  Character Count: {characterCount.postText}/280
-                </p>
-                <br />
-                <h6>What do you want in return?</h6>{" "}
-                <textarea
-                  id="expectedTradeCompensation"
-                  name="expectedTradeCompensation"
-                  placeholder="What do you want in exchange for this item? Multiple options may help you find a match faster."
-                  value={formState.expectedTradeCompensation}
-                  className="form-input w-100"
-                  style={{ lineHeight: "1.5", resize: "vertical" }}
-                  onChange={handleChange}
-                ></textarea>
-                <p
-                  className={`m-0 mb-2 ${
-                    characterCount.expectedTradeCompensation >= 150 || error
-                      ? "text-danger"
-                      : ""
-                  }`}
-                >
-                  Character Count: {characterCount.expectedTradeCompensation}
-                  /150
-                </p>
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+  if (!Auth.loggedIn()) {
+    <p>
+      You need to be logged in to list your items. Please log in or sign up
+      using the link in the nav bar.
+    </p>;
+  } else {
+    if (
+      post?.postAuthor._id?.toString() !==
+      Auth.getProfile()?.data?._id?.toString()
+    ) {
+      return <h2>You can't edit a post you didn't create!</h2>;
+    } else {
+      return (
+        <div className="mb-3">
+          <h3>Update your Listing</h3>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12">
+              <h6>The name of your listed item:</h6>
+              <textarea
+                id="postTitle"
+                name="postTitle"
+                placeholder="What item are you offering?"
+                value={formState.postTitle}
+                className="form-input w-100"
+                style={{ lineHeight: "1.5", resize: "vertical" }}
+                onChange={handleChange}
+              ></textarea>
+              <p
+                className={`m-0 ${
+                  characterCount.postTitle >= 150 || error ? "text-danger" : ""
+                }`}
+              >
+                Character Count: {characterCount.postTitle}/150
+              </p>
+              <br />
+              <h6>Listing Details:</h6>
+              <textarea
+                id="postText"
+                name="postText"
+                placeholder="Type any relevant information about your item here..."
+                value={formState.postText}
+                className="form-input w-100"
+                style={{ lineHeight: "1.5", resize: "vertical" }}
+                onChange={handleChange}
+              ></textarea>
+              <p
+                className={`m-0 ${
+                  characterCount.postText >= 280 || error ? "text-danger" : ""
+                }`}
+              >
+                Character Count: {characterCount.postText}/280
+              </p>
+              <br />
+              <h6>What do you want in return?</h6>{" "}
+              <textarea
+                id="expectedTradeCompensation"
+                name="expectedTradeCompensation"
+                placeholder="What do you want in exchange for this item? Multiple options may help you find a match faster."
+                value={formState.expectedTradeCompensation}
+                className="form-input w-100"
+                style={{ lineHeight: "1.5", resize: "vertical" }}
+                onChange={handleChange}
+              ></textarea>
+              <p
+                className={`m-0 mb-2 ${
+                  characterCount.expectedTradeCompensation >= 150 || error
+                    ? "text-danger"
+                    : ""
+                }`}
+              >
+                Character Count: {characterCount.expectedTradeCompensation}
+                /150
+              </p>
+            </div>
+            <div className="col-12">
+              <button
+                className="btn bg-success justify-content-right"
+                type="submit"
+              >
+                Update This Listing
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
               </div>
-              <div className="col-12">
-                <button
-                  className="btn bg-success justify-content-right"
-                  type="submit"
-                >
-                  Update This Listing
-                </button>
-              </div>
-              {error && (
-                <div className="col-12 my-3 bg-danger text-white p-3">
-                  {error.message}
-                </div>
-              )}
-            </form>
-          </>
-        )
-      ) : (
-        <p>
-          You need to be logged in to list your items. Please log in or sign up
-          using the link in the nav bar.
-        </p>
-      )}
-    </div>
-  );
+            )}
+          </form>
+        </div>
+      );
+    }
+  }
 };
 
 export default EditPostForm;
